e5dccc642a5e7d50cd749fb9c5967330dd5fb780
Bogdan Popescu <68062990+bopopescu@users.noreply.github.com>
2020-07-25 01:48:31 +0300

    Discard master/slave vocabulary

--- google-cloud-sdk/lib/surface/container/clusters/create.py
+++ google-cloud-sdk/lib/surface/container/clusters/create.py
@@ -253,7 +253,7 @@ def ParseCreateOptionsBase(args):
       enable_kubernetes_alpha=args.enable_kubernetes_alpha,
       enable_cloud_run_alpha=args.enable_cloud_run_alpha if args.IsSpecified('enable_cloud_run_alpha') else None,
       enable_legacy_authorization=args.enable_legacy_authorization,
-      enable_master_authorized_networks=args.enable_master_authorized_networks,
+      enable_main_authorized_networks=args.enable_main_authorized_networks,
       enable_network_policy=args.enable_network_policy,
       enable_private_nodes=args.enable_private_nodes,
       enable_private_endpoint=args.enable_private_endpoint,
@@ -268,8 +268,8 @@ def ParseCreateOptionsBase(args):
       maintenance_window_start=args.maintenance_window_start,
       maintenance_window_end=args.maintenance_window_end,
       maintenance_window_recurrence=args.maintenance_window_recurrence,
-      master_authorized_networks=args.master_authorized_networks,
-      master_ipv4_cidr=args.master_ipv4_cidr,
+      main_authorized_networks=args.main_authorized_networks,
+      main_ipv4_cidr=args.main_ipv4_cidr,
       max_nodes=args.max_nodes,
       max_nodes_per_pool=args.max_nodes_per_pool,
       min_cpu_platform=args.min_cpu_platform,
@@ -362,7 +362,7 @@ class Create(base.CreateCommand):
     flags.AddLabelsFlag(parser)
     flags.AddLocalSSDFlag(parser)
     flags.AddMaintenanceWindowGroup(parser)
-    flags.AddMasterAuthorizedNetworksFlags(parser)
+    flags.AddMainAuthorizedNetworksFlags(parser)
     flags.AddMinCpuPlatformFlag(parser)
     flags.AddNetworkPolicyFlags(parser)
     flags.AddNodeTaintsFlag(parser)
@@ -413,12 +413,12 @@ class Create(base.CreateCommand):
     options = self.ParseCreateOptions(args)
 
     if options.private_cluster and not (
-        options.enable_master_authorized_networks or
-        options.master_authorized_networks):
+        options.enable_main_authorized_networks or
+        options.main_authorized_networks):
       log.warning(
-          '`--private-cluster` makes the master inaccessible from '
+          '`--private-cluster` makes the main inaccessible from '
           'cluster-external IP addresses, by design. To allow limited '
-          'access to the master, see the `--master-authorized-networks` flags '
+          'access to the main, see the `--main-authorized-networks` flags '
           'and our documentation on setting up private clusters: '
           'https://cloud.google.com'
           '/kubernetes-engine/docs/how-to/private-clusters')
@@ -512,7 +512,7 @@ class CreateBeta(Create):
     flags.AddLabelsFlag(parser)
     flags.AddLocalSSDFlag(parser)
     flags.AddMaintenanceWindowGroup(parser)
-    flags.AddMasterAuthorizedNetworksFlags(parser)
+    flags.AddMainAuthorizedNetworksFlags(parser)
     flags.AddMinCpuPlatformFlag(parser)
     flags.AddNetworkPolicyFlags(parser)
     flags.AddNodeTaintsFlag(parser)
@@ -535,7 +535,7 @@ class CreateBeta(Create):
     flags.AddSurgeUpgradeFlag(parser, default=1)
     flags.AddMaxUnavailableUpgradeFlag(parser, is_create=True)
     flags.AddReservationAffinityFlags(parser)
-    flags.AddMasterGlobalAccessFlag(parser)
+    flags.AddMainGlobalAccessFlag(parser)
     flags.AddEnableGvnicFlag(parser)
     _AddReleaseChannelGroup(parser)
 
@@ -559,7 +559,7 @@ class CreateBeta(Create):
     ops.autoscaling_profile = args.autoscaling_profile
     ops.enable_tpu_service_networking = args.enable_tpu_service_networking
     ops.enable_logging_monitoring_system_only = args.enable_logging_monitoring_system_only
-    ops.enable_master_global_access = args.enable_master_global_access
+    ops.enable_main_global_access = args.enable_main_global_access
     ops.enable_gvnic = args.enable_gvnic
     return ops
 
@@ -588,7 +588,7 @@ class CreateAlpha(Create):
     flags.AddLabelsFlag(parser)
     flags.AddLocalSSDAndLocalSSDVolumeConfigsFlag(parser)
     flags.AddMaintenanceWindowGroup(parser)
-    flags.AddMasterAuthorizedNetworksFlags(parser)
+    flags.AddMainAuthorizedNetworksFlags(parser)
     flags.AddMinCpuPlatformFlag(parser)
     flags.AddNetworkPolicyFlags(parser)
     flags.AddILBSubsettingFlags(parser)
@@ -622,7 +622,7 @@ class CreateAlpha(Create):
     flags.AddCostManagementConfigFlag(parser)
     flags.AddReservationAffinityFlags(parser)
     flags.AddDatapathProviderFlag(parser)
-    flags.AddMasterGlobalAccessFlag(parser)
+    flags.AddMainGlobalAccessFlag(parser)
     flags.AddEnableGvnicFlag(parser)
 
   def ParseCreateOptions(self, args):
@@ -637,7 +637,7 @@ class CreateAlpha(Create):
     ops.private_cluster = args.private_cluster
     ops.enable_private_nodes = args.enable_private_nodes
     ops.enable_private_endpoint = args.enable_private_endpoint
-    ops.master_ipv4_cidr = args.master_ipv4_cidr
+    ops.main_ipv4_cidr = args.main_ipv4_cidr
     ops.enable_tpu_service_networking = args.enable_tpu_service_networking
     ops.istio_config = args.istio_config
     ops.identity_namespace = args.identity_namespace
@@ -660,7 +660,7 @@ class CreateAlpha(Create):
     ops.enable_cost_management = args.enable_cost_management
     ops.enable_logging_monitoring_system_only = args.enable_logging_monitoring_system_only
     ops.datapath_provider = args.datapath_provider
-    ops.enable_master_global_access = args.enable_master_global_access
+    ops.enable_main_global_access = args.enable_main_global_access
     ops.enable_gvnic = args.enable_gvnic
     return ops
 
